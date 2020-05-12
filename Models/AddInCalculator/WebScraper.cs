44d7015552412f8ad669a81580da2a1c2a7c8d86
TylerLucas116 <tjl25@zips.uakron.edu>
2020-05-12 14:03:11 -0400

    Initial commit

---
+++ Models/AddInCalculator/WebScraper.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AddInCalculator2._0.Models.AddInCalculator
+{
+    class WebScraper
+    {
+
+    }
+}
