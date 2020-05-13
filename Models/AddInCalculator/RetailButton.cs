c8bf7ee3f50fc036a6b1bf54935004973de4daf1
TylerLucas116 <tjl25@zips.uakron.edu>
2020-05-13 14:31:47 -0400

    Delete unnecessary Retailer code (previous commits were a mistake

--- Models/AddInCalculator/RetailButton.cs
+++ Models/AddInCalculator/RetailButton.cs
@@ -8,36 +8,6 @@ namespace AddInCalculator2._0.Models.AddInCalculator
 {
     public class RetailButton
     {
-        private string name;
-        private string websiteUrl;
-        private string onlineAbbrev;
-        private double onlinePrice;
-        private double onlinePercentage;
-
-        public string Name
-        {
-            get { return name; }
-            set { name = value; }
-        }
-        public string WebsiteURL
-        {
-            get { return websiteUrl; }
-            set { websiteUrl = value; }
-        }
-        public string OnlineAbbrev
-        {
-            get { return onlineAbbrev; }
-            set { onlineAbbrev = value; }
-        }
-        public double OnlinePrice
-        {
-            get { return onlinePrice; }
-            set { onlinePrice = value; }
-        }
-        private double OnlinePercentage
-        {
-            get { return onlinePercentage; }
-            set { onlinePercentage = value; }
-        }
+        
     }
 }
