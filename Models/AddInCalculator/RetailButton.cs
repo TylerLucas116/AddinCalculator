c84b53a8640258ace5b992aeb8ad8f7bee23911b
TylerLucas116 <tjl25@zips.uakron.edu>
2020-05-13 14:29:49 -0400

    Copy class Retailer

--- Models/AddInCalculator/RetailButton.cs
+++ Models/AddInCalculator/RetailButton.cs
@@ -6,8 +6,38 @@ using System.Threading.Tasks;
 
 namespace AddInCalculator2._0.Models.AddInCalculator
 {
-    class RetailButton
+    public class RetailButton
     {
+        private string name;
+        private string websiteURL;
+        private string onlineAbbrev;
+        private double onlinePrice;
+        private double onlinePercentage;
 
+        public string Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+        public string WebsiteURL
+        {
+            get { return websiteURL; }
+            set { websiteURL = value; }
+        }
+        public string OnlineAbbrev
+        {
+            get { return onlineAbbrev; }
+            set { onlineAbbrev = value; }
+        }
+        public double OnlinePrice
+        {
+            get { return onlinePrice; }
+            set { onlinePrice = value; }
+        }
+        private double OnlinePercentage
+        {
+            get { return onlinePercentage; }
+            set { onlinePercentage = value; }
+        }
     }
 }
