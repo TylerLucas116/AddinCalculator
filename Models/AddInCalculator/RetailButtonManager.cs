1df4143b279e59db950dd1028b1557ffffa0fddc
TylerLucas116 <tjl25@zips.uakron.edu>
2020-05-15 09:22:04 -0400

    Add OC<RetailButton> retailButton

--- Models/AddInCalculator/RetailButtonManager.cs
+++ Models/AddInCalculator/RetailButtonManager.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Collections.Specialized;
 using System.ComponentModel;
 using System.Linq;
@@ -10,6 +11,13 @@ namespace AddInCalculator2._0.Models.AddInCalculator
 {
     class RetailButtonManager : INotifyCollectionChanged, INotifyPropertyChanged
     {
+        private ObservableCollection<RetailButton> retailButtons = new ObservableCollection<RetailButton>();
+
+        public ObservableCollection<RetailButton> RetailButtons
+        {
+            get { return retailButtons; }
+            set { retailButtons = value; }
+        }
 
         public event PropertyChangedEventHandler PropertyChanged;
         protected void OnPropertyChanged(string name)
