6294b143e8e4f5d84ee58b9302151d26d8695d9f
TylerLucas116 <tjl25@zips.uakron.edu>
2020-05-15 09:18:36 -0400

    Add and implement INotifyCollectionChanged and INotifyPropertyChanged

--- Models/AddInCalculator/RetailButtonManager.cs
+++ Models/AddInCalculator/RetailButtonManager.cs
@@ -1,12 +1,31 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace AddInCalculator2._0.Models.AddInCalculator
 {
-    class RetailButtonManager
+    class RetailButtonManager : INotifyCollectionChanged, INotifyPropertyChanged
     {
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        protected void OnPropertyChanged(string name)
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(name));
+            }
+        }
+        public event NotifyCollectionChangedEventHandler CollectionChanged;
+        private void OnCollectionChanged(NotifyCollectionChangedEventArgs args)
+        {
+            if (this.CollectionChanged != null)
+            {
+                this.CollectionChanged(this, args);
+            }
+        }
     }
 }
