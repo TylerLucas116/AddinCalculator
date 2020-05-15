64931e55cbcf3e7a10cf49f06840d100c9ff31ef
TylerLucas116 <tjl25@zips.uakron.edu>
2020-05-15 19:11:20 -0400

    Add code from ButtonSettingsViewModel

--- ViewModels/RetailButtonSettingsViewModel.cs
+++ ViewModels/RetailButtonSettingsViewModel.cs
@@ -3,10 +3,19 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using AddInCalculator2._0.Models.AddInCalculator;
 
 namespace AddInCalculator2._0.ViewModels
 {
     class RetailButtonSettingsViewModel
     {
+        private static ButtonManager bManager = new ButtonManager();
+        public static ButtonManager BManager { get { return bManager; } }
+
+        public RetailButtonSettingsViewModel()
+        {
+            bManager = new ButtonManager();
+            bManager.UpdateButtons();
+        }
     }
 }
