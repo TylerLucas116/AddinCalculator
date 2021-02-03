0e4728aead271ac4b81b4ea012b94c1a1b26d1ff
TylerLucas116 <tjl25@zips.uakron.edu>
2021-02-03 11:05:03 -0500

    Add documentation for Divide()

--- Models/AddInCalculator/Calculator.cs
+++ Models/AddInCalculator/Calculator.cs
@@ -182,6 +182,11 @@ namespace AddInCalculator2._0.Models.AddInCalculator
                 HasDecimal = false;
         }
 
+        /// <summary>
+        /// Logically represents the user pressing the divide '/' button in the UI calculator
+        /// </summary>
+        /// <param name="sender">The '/' button in the UI calculator</param>
+        /// <param name="e"></param>
         public void Divide(object sender, RoutedEventArgs e)
         {
             if (DisplayText == "" || DisplayText == ".")
