173d78a45a7219d719c2bf4d1478cab9fc0352b5
TylerLucas116 <tjl25@zips.uakron.edu>
2021-02-03 11:05:37 -0500

    Add documentation for Multiply()

--- Models/AddInCalculator/Calculator.cs
+++ Models/AddInCalculator/Calculator.cs
@@ -199,6 +199,11 @@ namespace AddInCalculator2._0.Models.AddInCalculator
             HasDecimal = false;
         }
 
+        /// <summary>
+        /// Logically represents the user pressing the divide '*' button in the UI calculator
+        /// </summary>
+        /// <param name="sender">The '*' button in the UI calculator</param>
+        /// <param name="e"></param>
         public void Multiply(object sender, RoutedEventArgs e)
         {
             if (DisplayText == "" || DisplayText == ".")
